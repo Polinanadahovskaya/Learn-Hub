c01396fe94541b4ed80106db41b6d102d33b0a29
Polinanadahovskaya <Polinanadahovskaya@gmail.com>
2022-10-19 17:04:51 +0300

    '2'

--- script2.js
+++ script2.js
@@ -8,18 +8,22 @@ let sixNumber = document.getElementsByClassName('tel').slice(9, 11);
 let numTel = document.getElementsByClassName('tel')
 
 
-let result =[]
+// let result =[]
 
-if (firstNumber === 8 || firstNumber === 7){
-    firstNumber = '+';
-    secondNumber = 7;
-    let a  = `${firstNumber}${secondNumber}(${telNumber})${fourNumber}-${fiveNumber}-${sixNumber}`
-    result.push(a)
-} else if (firstNumber === '+'){
-    result.push(numTel)
-} else if(numTel !== 'number' || numTel.length !== 12 || numTel.length === 0){
-    return null
-} 
+// if (firstNumber === 8 || firstNumber === 7){
+//     firstNumber = '+';
+//     secondNumber = 7;
+//     let a  = `${firstNumber}${secondNumber}(${telNumber})${fourNumber}-${fiveNumber}-${sixNumber}`
+//     result.push(a)
+// } else if (firstNumber === '+'){
+//     result.push(numTel)
+// } else if(numTel !== 'number' || numTel.length !== 12 || numTel.length === 0){
+//     return null
+// } 
 
-return `привет: ${result.toString()}`
+// return `привет: ${result.toString()}`
 
+// const number = numTel; 
+// const pattern = new RegExp(`(^\\d{${number.length % 10}})(\\d{3})(\\d{3})(\\d{2})(\\d{2}$)`);
+// const formattedNumber = number.replace(pattern, '+$1($2)$3-$4-$5');
+// console.log(formattedNumber); 
--- script4.js
+++ script4.js
@@ -14,7 +14,7 @@ function formValueEntry (event) {
 
 
     console.log(values2)
-    
+
 
     
 }
