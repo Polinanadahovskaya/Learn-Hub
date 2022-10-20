1ea2976ebbfca5868c6f9b1993d78948b267e341
Polinanadahovskaya <Polinanadahovskaya@gmail.com>
2022-10-20 14:17:47 +0300

    '4'

--- script4.js
+++ script4.js
@@ -1,5 +1,8 @@
 
 const forma = document.querySelector('.form_singin');
+const but = document.querySelector('.form__button_singin');
+
+import { values1 } from "./script3";
 
 export const values2 = {}
 
@@ -15,8 +18,24 @@ function formValueEntry (event) {
 
     console.log(values2)
 
-
     
+
+
+function button(event)
+{ event.preventDefault();
+    if(values1.login === values2.login && values1.password === values2.password){
+
+        console.log(true)
+        return true
+    } else {
+        return false
+    }
+}
+
+
+but.addEventListener('submit', button)
+
+
 }
 
 forma.addEventListener('submit', formValueEntry)
