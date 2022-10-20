4fb280176e9f0d64baab4430ddad4f1ff2574754
Polinanadahovskaya <Polinanadahovskaya@gmail.com>
2022-10-20 13:55:29 +0300

    '3'

--- script2.js
+++ script2.js
@@ -1,11 +1,11 @@
-let firstNumber = document.getElementsByClassName('tel').slice(0, 1);
-let secondNumber;
-let telNumber = document.getElementsByClassName('tel').slice(1);
-let threeNumber = document.getElementsByClassName('tel').slice(1, 4);
-let fourNumber = document.getElementsByClassName('tel').slice(4, 7);
-let fiveNumber = document.getElementsByClassName('tel').slice(7, 9);
-let sixNumber = document.getElementsByClassName('tel').slice(9, 11);
-let numTel = document.getElementsByClassName('tel')
+// let firstNumber = document.getElementsByClassName('tel').slice(0, 1);
+// let secondNumber;
+// let telNumber = document.getElementsByClassName('tel').slice(1);
+// let threeNumber = document.getElementsByClassName('tel').slice(1, 4);
+// let fourNumber = document.getElementsByClassName('tel').slice(4, 7);
+// let fiveNumber = document.getElementsByClassName('tel').slice(7, 9);
+// let sixNumber = document.getElementsByClassName('tel').slice(9, 11);
+let numTel = document.querySelector('.tel')
 
 
 // let result =[]
@@ -23,7 +23,7 @@ let numTel = document.getElementsByClassName('tel')
 
 // return `привет: ${result.toString()}`
 
-// const number = numTel; 
-// const pattern = new RegExp(`(^\\d{${number.length % 10}})(\\d{3})(\\d{3})(\\d{2})(\\d{2}$)`);
-// const formattedNumber = number.replace(pattern, '+$1($2)$3-$4-$5');
-// console.log(formattedNumber); 
+const number = numTel; 
+const pattern = new RegExp(`(^\\d{${number.length % 10}})(\\d{3})(\\d{3})(\\d{2})(\\d{2}$)`);
+const formattedNumber = number.replace(pattern, '+$1($2)$3-$4-$5');
+console.log(formattedNumber); 
--- script5.js
+++ script5.js
@@ -1,13 +1,18 @@
 import { values1 } from "./script3";
 import { values2 } from "./script4";
 
+const but = document.querySelector('.form__button_singin')
 
- export function proverka() {
+function button(event)
+{ event.preventDefault();
     if(values1.login === values2.login && values1.password === values2.password){
 
-    console.log(true)
-    return true
-} else {
-    return false
+        console.log(true)
+        return true
+    } else {
+        return false
+    }
 }
-}
+
+
+but.addEventListener('submit', button)
